9f210f1614ef1a03ad85a05b6607eab72bd854fe
Tidbits JS <tidbitsjs@gmail.com>
2022-08-12 00:18:54 +0530

    roboto font family

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
 
 import Tabs from "./navigation/tabs";
 import themeReducer from "./store/themeReducer";
@@ -14,6 +15,16 @@ const Stack = createStackNavigator();
 const store = createStore(themeReducer, applyMiddleware(thunk));
 
 const App = () => {
+  const [loaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
